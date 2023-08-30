61f0f3339e6a44d2465afe5a08b6c1489dffd1ee
Polarisguides <liguoqing.guangxi@qq.com>
2023-08-30 16:09:08 +0800

    Site updated: 2023-08-30 16:09:08

--- js/runtime.js
+++ js/runtime.js
@@ -28,8 +28,9 @@ function createtime() {
       , b = Math.round(g);
     1 == String(b).length && (b = "0" + b);
     let c = "";
-    c = s < 18 && s >= 9 ? `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-科研摸鱼中.svg' title='什么时候能够实现财富自由呀~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 星际者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>` : `<img class='boardsign' src='https://sourcebucket.s3.ladydaily.com/badge/F小屋-下班休息啦.svg' title='下班了就该开开心心地玩耍~'><br> <div style="font-size:13px;font-weight:bold">本站居然运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 星际者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>`,
-    document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = c)
+    c = s < 18 && s >= 9 ? `<div style="font-size:13px;font-weight:bold">本站居然运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 星际者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>` : `<div style="font-size:13px;font-weight:bold">本站居然运行了 ${r} 天 ${s} 小时 ${l} 分 ${b} 秒 <i id="heartbeat" class='fas fa-heartbeat'></i> <br> 星际者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</div>`;
+
+    document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = c);
 }
 setInterval((()=>{
     createtime()
